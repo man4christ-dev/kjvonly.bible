6fdb0360cf6a8972d6cdd9204cca3b05129c7505
man4christ <man4christ.65qa8@simplelogin.com>
2025-09-27 18:29:02 -0700

    add color theme circles to settings

--- api/frontends/bible/src/lib/modules/chapter/settingsPopup.svelte
+++ api/frontends/bible/src/lib/modules/chapter/settingsPopup.svelte
@@ -185,7 +185,7 @@
 				</button>
 			</div>
 
-			<div class="flex w-full flex-row bg-neutral-50 p-4 font-bold">
+			<div class="flex w-full flex-col bg-neutral-50 p-4 font-bold">
 				{#if settings?.colorTheme}
 					<select
 						name="HeadlineAct"
@@ -203,6 +203,30 @@
 						<option value="pink">PINK</option>
 					</select>
 				{/if}
+				<div class="felx w-full pt-2">
+					<div class="flex justify-center space-x-4">
+						<button
+							aria-label="color-a"
+							class="bg-highlighta h-8 w-8 rounded-full"
+						></button>
+						<button
+							aria-label="color-b"
+							class="bg-highlightb h-8 w-8 rounded-full"
+						></button>
+						<button
+							aria-label="color-c"
+							class="bg-highlightc h-8 w-8 rounded-full"
+						></button>
+						<button
+							aria-label="color-d"
+							class="bg-highlightd h-8 w-8 rounded-full"
+						></button>
+						<button
+							aria-label="color-e"
+							class="bg-highlighte h-8 w-8 rounded-full"
+						></button>
+					</div>
+				</div>
 			</div>
 
 			<div class="p-4">
