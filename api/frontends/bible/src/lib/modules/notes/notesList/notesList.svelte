eb8bc2cda99ee33975307ab5c8adf7021d760995
man4christ <man4christ.65qa8@simplelogin.com>
2025-10-19 16:25:00 -0700

    refactor note list header

--- api/frontends/bible/src/lib/modules/notes/notesList/notesList.svelte
+++ api/frontends/bible/src/lib/modules/notes/notesList/notesList.svelte
@@ -18,6 +18,9 @@
 	import Bible from '$lib/components/svgs/bible.svelte';
 	import SplitScreenBottom from '$lib/components/svgs/splitScreenBottom.svelte';
 	import SplitScreenRight from '$lib/components/svgs/splitScreenRight.svelte';
+	import AddNote from '$lib/components/svgs/addNote.svelte';
+	import Menu from '$lib/components/svgs/menu.svelte';
+	import Close from '$lib/components/svgs/close.svelte';
 
 	// =============================== BINDINGS ================================
 
@@ -208,90 +211,33 @@
 			noteID: noteID
 		});
 	}
+
+	function onClose(): void {
+		if (allNotes) {
+			paneService.onDeletePane(paneService.rootPane, mode.paneID);
+		} else {
+			mode.notePopup.show = false;
+		}
+	}
 </script>
 
 <!-- ================================ HEADER =============================== -->
 {#snippet noteListHeader()}
-	<header
-		bind:clientHeight={headerHeight}
-		class=" flex w-full max-w-lg flex-row items-center justify-between bg-neutral-100 text-neutral-700"
+	<KJVButton onClick={onAdd} classes="">
+		<AddNote></AddNote>
+	</KJVButton>
+
+	<span></span>
+	<span class="">Notes</span>
+	<KJVButton
+		classes=""
+		onClick={() => (showNoteListActions = !showNoteListActions)}
 	>
-		<button
-			aria-label="close"
-			onclick={() => {
-				onAdd();
-			}}
-			class="h-12 w-12 px-2 pt-2 text-neutral-700"
-		>
-			<svg
-				class="p-0.5"
-				version="1.1"
-				width="100%"
-				height="100%"
-				viewBox="0 0 105.50072 106.78786"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<g id="g8" transform="translate(-11.214067,-10.602166)">
-					<path
-						class="fill-neutral-700"
-						style="stroke-width:4.20363;stroke-linejoin:round"
-						d="M 63.952348,10.627557 A 52.737736,53.368481 0 0 0 11.214067,63.996697 52.737736,53.368481 0 0 0 63.952348,117.36388 52.737736,53.368481 0 0 0 116.68868,63.996697 52.737736,53.368481 0 0 0 63.952348,10.627557 Z m -4.40625,34.925781 h 8.884766 v 14.335937 h 12.917969 v 8.138672 H 68.430864 V 82.438103 H 59.546098 V 68.027947 H 46.553911 v -8.138672 h 12.992187 z"
-					/>
-				</g>
-			</svg>
-		</button>
-		<!-- svelte-ignore a11y_click_events_have_key_events -->
-		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-		<p
-			onclick={() => {
-				showNoteListActions = !showNoteListActions;
-			}}
-			class="hover:cursor-pointer"
-		>
-			<span class="inline-block font-bold">Notes</span>
-			<button aria-label="chevron down" class="h-4 w-4">
-				<svg
-					width="100%"
-					height="100%"
-					viewBox="0 0 25.4 14.098638"
-					version="1.1"
-					xml:space="preserve"
-					xmlns="http://www.w3.org/2000/svg"
-					><g transform="translate(-53.644677,-127.79211)"
-						><path
-							class="fill-neutral-700"
-							style="stroke-width:0.352778"
-							d="m 59.906487,137.65245 -6.26181,-4.21622 v -2.82206 -2.82206 l 6.35,4.24282 6.35,4.24283 6.35,-4.24283 6.35,-4.24282 v 2.82222 2.82222 l -6.3429,4.23808 c -3.48859,2.33094 -6.38578,4.22817 -6.43819,4.21606 -0.0524,-0.0121 -2.91311,-1.91931 -6.3571,-4.23824 z"
-							id="path179"
-						/></g
-					></svg
-				>
-			</button>
-		</p>
-		<button
-			aria-label="close"
-			onclick={() => {
-				if (allNotes) {
-					paneService.onDeletePane(paneService.rootPane, mode.paneID);
-				} else {
-					mode.notePopup.show = false;
-				}
-			}}
-			class="h-12 w-12 px-2 pt-2 text-neutral-700"
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 24"
-				width="100%"
-				height="100%"
-			>
-				<path
-					class="fill-neutral-700"
-					d="M12,2C6.47,2,2,6.47,2,12s4.47,10,10,10s10-4.47,10-10S17.53,2,12,2z M17,15.59L15.59,17L12,13.41L8.41,17L7,15.59 L10.59,12L7,8.41L8.41,7L12,10.59L15.59,7L17,8.41L13.41,12L17,15.59z"
-				/>
-			</svg>
-		</button>
-	</header>
+		<Menu></Menu>
+	</KJVButton>
+	<KJVButton classes="" onClick={onClose}>
+		<Close></Close>
+	</KJVButton>
 {/snippet}
 
 <!-- ================================= BODY ================================ -->
